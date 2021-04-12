da714ea1aa996ec95e423e14d049e50b8493aaa3
RufailG <girgis.rufail@iisgalvanimi.edu.it>
2021-04-12 10:13:19 +0000

    origin client

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ const serverless = require ('serverless-http');
 var chatRouter = require('./routes/chat');
 const cors = require('cors');
 const app = express();
-app.use(cors({credentials: true, origin: 'https://chatserver.netlify.app/'}));
+app.use(cors({credentials: true, origin: 'https://4200-green-rook-ix0n3quq.ws-eu03.gitpod.io/'}));
 const router = express.Router();
 
  //E' necessario usare per tutte le route il prefisso /.netlify/functions
